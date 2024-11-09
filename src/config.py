403f3a5973782de947d16289bca4bc6f46da1c6e
naina-goel <naina.goel15@gmail.com>
2024-11-09 10:18:44 +0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ class Config:
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
     DATABASE_NAME = "clinical_trials_db"
     COLLECTION_NAME = "clinical_trials"
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "${OPEN_API_KEY}")
     API_URL = "https://clinicaltrials.gov/api/query/study_fields"
     API_PARAMS = {
         "expr": "LASTUPDATE_DATE:[2024-10-20+TO+2024-10-21]",
