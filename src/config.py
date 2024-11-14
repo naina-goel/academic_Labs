019c785d8f816c677aee07313ca9eb0a84b7a8aa
naina-goel <naina.goel15@gmail.com>
2024-11-14 18:47:50 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class Config:
     DATABASE_NAME = "clinical_trials_db"
     COLLECTION_NAME = "clinical_trials"
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "${OPEN_API_KEY}")
-    API_URL = "https://clinicaltrials.gov/api/query/study_fields"
+    API_URL = "https://clinicaltrials.gov/api/v2/studies"
     API_PARAMS = {
         "expr": "LASTUPDATE_DATE:[2024-10-20+TO+2024-10-21]",
         "fields": "NCTId,Title,Phase,StartDate,CompletionDate,LeadSponsorName,LocationFacility,EligibilityCriteria",
